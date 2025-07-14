ba7505de33d3c6156aafbe38ed9063cc243cc173
rahul-pandey-17 <rahul.0517@yahoo.com>
2025-07-14 11:44:37 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
 import authRoutes from './routes/auth/auth'
 import blogRoutes from './routes/blog/blog'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use('*', cors())
 
 app.route('/auth', authRoutes)
 app.route('/blog', blogRoutes)
